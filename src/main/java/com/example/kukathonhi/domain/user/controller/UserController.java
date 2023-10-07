faa8810c801ae8da0a65f922776cb62c1de58ef7
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-07 14:39:02 +0900

    [feat] 유저 도메인, 로그인 / 회원가입

---
+++ src/main/java/com/example/kukathonhi/domain/user/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.kukathonhi.domain.user.controller;
+
+import com.example.kukathonhi.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+}
--- src/main/java/com/example/kukathonhi/domain/user/entity/User.java
+++ src/main/java/com/example/kukathonhi/domain/user/entity/User.java
@@ -1,4 +1,29 @@
 package com.example.kukathonhi.domain.user.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Entity(name = "user")
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
 }
