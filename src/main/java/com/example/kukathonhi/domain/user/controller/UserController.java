99df9dfeeff76cf396ff24fa739782f30eb16a14
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-07 20:54:41 +0900

    [feat] 유저 api 작성 (로그인, 로그아웃, 정보조회)

--- src/main/java/com/example/kukathonhi/domain/user/controller/UserController.java
+++ src/main/java/com/example/kukathonhi/domain/user/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.example.kukathonhi.domain.user.controller;
 
+import com.example.kukathonhi.domain.user.dto.req.RegisterRequestDto;
 import com.example.kukathonhi.domain.user.service.UserService;
+import com.example.kukathonhi.global.response.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -12,5 +13,25 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("")
+    public BaseResponseDto<?> register(
+            @RequestBody RegisterRequestDto requestDto
+            ) {
+        return userService.join(requestDto);
+    }
 
+    @GetMapping("/login")
+    public BaseResponseDto<?> login(
+            @RequestParam String id,
+            @RequestParam String password
+    ) {
+        return userService.login(id, password);
+    }
+
+    @GetMapping("/{userId}")
+    public BaseResponseDto<?> getUserInfo(
+            @PathVariable Long userId
+    ) {
+        return userService.getUserInfo(userId);
+    }
 }
