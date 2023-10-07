68ba3abe33459c4c5c97e15f303feee41e61d1ea
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-07 10:39:57 +0900

    initial commit

---
+++ src/main/java/com/example/kukathonhi/domain/user/entity/User.java
@@ -0,0 +1,4 @@
+package com.example.kukathonhi.domain.user.entity;
+
+public class User {
+}
---
+++ src/test/java/com/example/kukathonhi/KukathonHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kukathonhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukathonHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
